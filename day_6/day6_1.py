8385204ac471b74fffacb3f08a82abbd425425cf
Caelyn McAulay <caelyn@ceruleanrodent.com>
2015-12-17 08:58:03 -0500

    solution for day 6

---
+++ day_6/day6_1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+instructions = open('input.txt', 'r').readlines()
+
+lights = {}
+
+count = 0
+
+for i in instructions:
+  if i[0:6] == 'toggle':
+    a = i[6:]
+    action = 'tog'
+  if i[0:7] == 'turn on':
+    a = i[7:]
+    action = 'on'
+  if i[0:8] == 'turn off':
+    a = i[8:]
+    action = 'off'
+
+  a = a.split('through')
+  a = [x.split(',') for x in a]
+  lil_x = int(a[0][0])
+  lil_y = int(a[0][1])
+  big_x = int(a[1][0])
+  big_y = int(a[1][1])
+
+  for x in xrange(lil_x, big_x+1):
+    for y in xrange(lil_y, big_y+1):
+      cord = str(x) + '_' + str(y)
+      if action == 'on':
+        lights[cord] = 1
+      elif action == 'off':
+        lights[cord] = 0
+      elif action == 'tog':
+        if cord not in lights:
+          lights[cord] = 1
+        elif lights[cord] == 0:
+          lights[cord] = 1
+        elif lights[cord] == 1:
+          lights[cord] = 0
+
+
+on = sum(lights.values())
+print on
